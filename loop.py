1d80b309d35c441ce919f9804222ac2fab126493
Your Name <pardipshahi17@gmail.com>
2024-05-17 07:41:52 +0545

    loop

---
+++ loop.py
@@ -0,0 +1,19 @@
+# name = "hello"
+# for i in range(len(name)):
+#   print(name[i])
+#   print(i)
+#
+# for item in range (5,10):
+#     print(item)
+#     print("a")
+# print("hello")
+
+# for item in range (5,10,7):
+#     print(item)
+
+
+price =[200, 300, 500]
+total=0
+for i in price:
+    total += i
+print(total)
